b957d85d5472ff05182104f4c8f5b3201a169e6b
GitArtOS <platojobs@qq.com>
2024-04-11 11:20:35 +0800

    组件化

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/home_page.dart';
+//import 'package:flutter_application_1/home_page.dart';
 import 'package:flutter_application_1/home_content.dart';
 
 void main() {
