55339142af7f587516011bb1a513af933514025f
GitArtOS <platojobs@qq.com>
2024-05-08 14:17:33 +0800

    add

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,25 @@
 
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/sliver_customScrollView.dart';
-
+import 'package:flutter_application_1/parallaxRecipe.dart';
+const Color darkBlue = Color.fromARGB(255, 18, 32, 47);
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
+  
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: darkBlue),
       home:  Scaffold(
       appBar: AppBar(
         title: const Text("Flutter Demo"),
       ),
-      body: const Sliver_customScrollView(),
+      body:  ParallaxRecipe(),
       ),
     );
   }
