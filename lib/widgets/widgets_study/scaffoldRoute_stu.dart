9976cefbd138a929380a431f4ccef0acd2d0d37c
GitArtOS <platojobs@qq.com>
2024-07-19 17:58:12 +0800

    add

---
+++ lib/widgets/widgets_study/scaffoldRoute_stu.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ScaffoldRoute_stu extends StatefulWidget {
+  const ScaffoldRoute_stu({super.key});
+
+  @override
+  State<ScaffoldRoute_stu> createState() => _ScaffoldRoute_stuState();
+}
+
+class _ScaffoldRoute_stuState extends State<ScaffoldRoute_stu> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(""),
+      ),
+    );
+  }
+}
